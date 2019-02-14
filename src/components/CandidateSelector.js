769d9857700ccaf6f2c88304554dbca636229b3a
Nathan Marley <nathan.marley@gmail.com>
2019-02-14 14:44:01 -0200

    testing crap

--- src/components/CandidateSelector.js
+++ src/components/CandidateSelector.js
@@ -1,81 +1,42 @@
 import React from 'react';
+import { Dropdown } from 'semantic-ui-react';
 
 class CandidateSelector extends React.Component {
-  this.state = { selected: [] };
+  state = { selected: [] };
+
+  onCSChange = selected => {
+    console.log(selected);
+    this.setState({ selected });
+  };
 
   render() {
+    const options = [
+      { label: 'Setzer Gabbiani', value: 'sgabbiani' },
+      { label: 'Cyan Garamonde', value: 'cgaramonde' },
+      { label: 'Edgar Figaro', value: 'efigaro' },
+      { label: 'Clyde Arrowny', value: 'carrowny' },
+      { label: 'Locke Cole', value: 'lcole' },
+      { label: 'Sabin Figaro', value: 'sfigaro' },
+      { label: 'Relm Arrowny', value: 'rarrowny' },
+      { label: 'Strago Magus', value: 'smagus' },
+      { label: 'Celes Chere', value: 'cchere' },
+      { label: 'Terra Branford', value: 'tbranford' },
+      { label: 'Leo Cristophe', value: 'lcristophe' },
+      { label: 'Kefka Palazzo', value: 'kpalazzo' },
+    ];
+
     return (
-      <div class="ui selection dropdown">
-
-<select class="ui fluid search dropdown" multiple="">
-  <option value="">State</option>
-  <option value="AL">Alabama</option>
-  <option value="AK">Alaska</option>
-  <option value="AZ">Arizona</option>
-  <option value="AR">Arkansas</option>
-  <option value="CA">California</option>
-  <option value="CO">Colorado</option>
-  <option value="CT">Connecticut</option>
-  <option value="DE">Delaware</option>
-  <option value="DC">District Of Columbia</option>
-  <option value="FL">Florida</option>
-  <option value="GA">Georgia</option>
-  <option value="HI">Hawaii</option>
-  <option value="ID">Idaho</option>
-  <option value="IL">Illinois</option>
-  <option value="IN">Indiana</option>
-  <option value="IA">Iowa</option>
-  <option value="KS">Kansas</option>
-  <option value="KY">Kentucky</option>
-  <option value="LA">Louisiana</option>
-  <option value="ME">Maine</option>
-  <option value="MD">Maryland</option>
-  <option value="MA">Massachusetts</option>
-  <option value="MI">Michigan</option>
-  <option value="MN">Minnesota</option>
-  <option value="MS">Mississippi</option>
-  <option value="MO">Missouri</option>
-  <option value="MT">Montana</option>
-  <option value="NE">Nebraska</option>
-  <option value="NV">Nevada</option>
-  <option value="NH">New Hampshire</option>
-  <option value="NJ">New Jersey</option>
-  <option value="NM">New Mexico</option>
-  <option value="NY">New York</option>
-  <option value="NC">North Carolina</option>
-  <option value="ND">North Dakota</option>
-  <option value="OH">Ohio</option>
-  <option value="OK">Oklahoma</option>
-  <option value="OR">Oregon</option>
-  <option value="PA">Pennsylvania</option>
-  <option value="RI">Rhode Island</option>
-  <option value="SC">South Carolina</option>
-  <option value="SD">South Dakota</option>
-  <option value="TN">Tennessee</option>
-  <option value="TX">Texas</option>
-  <option value="UT">Utah</option>
-  <option value="VT">Vermont</option>
-  <option value="VA">Virginia</option>
-  <option value="WA">Washington</option>
-  <option value="WV">West Virginia</option>
-  <option value="WI">Wisconsin</option>
-  <option value="WY">Wyoming</option>
-</select>
-
-      </div>
+      <Dropdown
+        placeholder="Select your candidates"
+        fluid
+        multiple
+        selection
+        options={options}
+        selected={this.state.selected}
+        onChange={this.onCSChange}
+      />
     );
-  }  
-};
+  }
+}
 
 export default CandidateSelector;
-
-<div class="ui selection dropdown">
-    <input type="hidden" name="gender">
-    <i class="dropdown icon"></i>
-    <div class="default text">Select Gender</div>
-    <div class="menu">
-        <div class="item" data-value="0">Male</div>
-        <div class="item" data-value="1">Female</div>
-        <div class="item" data-value="2">Other</div>
-    </div>
-</div>
