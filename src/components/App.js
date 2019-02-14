77bf23e5ee61e9438c36f929ef8421683a5a8add
Nathan Marley <nathan.marley@gmail.com>
2019-02-14 13:14:41 -0200

    flesh out

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+      </div>
+    );
+  }
+}
+
+export default App;
