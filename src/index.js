ecb5a30e8eca224d041345ddba8f3f86a458b120
Nathan Marley <nathan.marley@gmail.com>
2019-05-25 12:41:30 -0300

    Open voting back up

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './components/App';
-import Closed from './components/Closed';
+import App from './components/App';
+// import Closed from './components/Closed';
 
-ReactDOM.render(<Closed />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
